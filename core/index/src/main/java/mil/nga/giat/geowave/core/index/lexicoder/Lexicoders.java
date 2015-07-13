16942e3e0e34624fcb82af5319b56f77c562b3df
Jacob Meisler <jacob.meisler@gmail.com>
2015-07-13 18:02:29 -0400

    Implemented Number Lexicoders
    Implemented lexicoders for signed shorts, ints, and longs. The
    most significant bit is inverted so that negative numbers sort
    before positive numbers. Lexicographic sorting of the byte
    arrays matches the natural order of the numbers.

---
+++ core/index/src/main/java/mil/nga/giat/geowave/core/index/lexicoder/Lexicoders.java
@@ -0,0 +1,8 @@
+package mil.nga.giat.geowave.core.index.lexicoder;
+
+public class Lexicoders
+{
+	public static final ShortLexicoder SHORT = new ShortLexicoder();
+	public static final IntegerLexicoder INT = new IntegerLexicoder();
+	public static final LongLexicoder LONG = new LongLexicoder();
+}
